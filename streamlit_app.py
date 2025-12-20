f80fb75ad6336e9a49dc88042755d71a01ba9cc3
TimurMingazov <59471497+TimurMingazov@users.noreply.github.com>
2025-12-20 22:15:00 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,14 @@ from laba1 import run as run_laba1
 from laba2 import run as run_laba2
 from laba3 import run as run_laba3
 from dz_2_evd import run as run_dz2_evd
+from dz_3_evd import run as run_dz3_evd
 
 def main():
     st.title("Выбор работы")
 
     option = st.selectbox(
         "Выберите лабораторную работу",
-        ("Лабораторная 1 ДСП", "Лабораторная 2 ДСП", "Лабораторная 3 ДСП", "Домашняя работа 2 Технология приборостроения")
+        ("Лабораторная 1 ДСП", "Лабораторная 2 ДСП", "Лабораторная 3 ДСП", "Домашняя работа 2 Технология приборостроения", "Домашняя работа 3 Технология приборостроения")
     )
 
     if option == "Лабораторная 1 ДСП":
@@ -20,8 +21,11 @@ def main():
         run_laba3()
     elif option == "Домашняя работа 2 Технология приборостроения":
         run_dz2_evd()
+    elif option == "Домашняя работа 3 Технология приборостроения":
+        run_dz3_evd()
 
 if __name__ == "__main__":
     main()
+
 
 
